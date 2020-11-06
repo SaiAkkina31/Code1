a77a532ab89f2372c468b657a5e7c21ca7af1155
SaiAkkina31 <74037085+SaiAkkina31@users.noreply.github.com>
2020-11-06 10:56:51 +0530

    Create secondassignment.cpp
    C++ Program to Multiply Two numbers from user.

---
+++ secondassignment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout << "Enter to numbers:";
+	cin >> a >> b;
+	c=a*b;
+	cout << "Product = " << c;
+	return 0;
+}
