1b8caeac9104e1abc384f79b84abd0e356c264ba
SaiAkkina31 <74037085+SaiAkkina31@users.noreply.github.com>
2020-11-26 12:51:35 +0530

    Create vote.cpp
    This C++ program displays whether a person's age is sufficient or not to caste a vote

---
+++ vote.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int age;
+ 	
+ 	
+ 	cout<<"Enter your Age"<<endl;
+ 	cin>>age;
+ 	
+ 	if(age<18)
+ 	{
+ 		cout<<"You are not eligible for casting your vote"<<endl ;
+	 }
+	 else
+	 {
+	 	cout<<"You can vote !"<<endl;
+	 }
+ }
